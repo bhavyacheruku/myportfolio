7b52676cf71bc346b4e772d283c3fb2a45380de9
Bhavya Cheruku <bhavya8182@gmail.com>
2019-08-11 13:04:46 -0400

    Update precache-manifest.3717903b26fd0c3410d770ca7d6440a4.js

--- precache-manifest.3717903b26fd0c3410d770ca7d6440a4.js
+++ precache-manifest.3717903b26fd0c3410d770ca7d6440a4.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "7441e9fbbc9cab5c3d89",
-    "url": "/bhavyacheruku/static/js/runtime~main.7441e9fb.js"
+    "url": "/myportfolio/static/js/runtime~main.7441e9fb.js"
   },
   {
     "revision": "dafa5e046b2c385e011b",
-    "url": "/bhavyacheruku/static/js/main.dafa5e04.chunk.js"
+    "url": "/myportfolio/static/js/main.dafa5e04.chunk.js"
   },
   {
     "revision": "5ec21d738fba382f2658",
-    "url": "/bhavyacheruku/static/js/2.5ec21d73.chunk.js"
+    "url": "/myportfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
     "revision": "dafa5e046b2c385e011b",
-    "url": "/bhavyacheruku/static/css/main.a8c9d8d5.chunk.css"
+    "url": "/myportfolio/static/css/main.a8c9d8d5.chunk.css"
   },
   {
     "revision": "a3d0d9e1fbcac363129586bb82b4d465",
-    "url": "/bhavyacheruku/index.html"
+    "url": "/myportfolio/index.html"
   }
 ];
